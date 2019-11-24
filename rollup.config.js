a891802d8fc4ff70c7961b8d6de4620e69741204
chriswynn <chris@wynncodes.com>
2019-11-23 23:13:57 -0500

    feat: add postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
+import postcss from "rollup-plugin-postcss";
+
 export default {
   input: "src/js/index.js",
   output: {
     file: "public/bundle.js",
     format: "iife"
-  }
+  },
+  plugins: [
+    postcss({
+      modules: true,
+      plugins: []
+    })
+  ]
 };
